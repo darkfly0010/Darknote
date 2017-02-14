d9db8ff1c6361bf77c33f2bf43512612676b9ada
Aritra <darkfly0010@gmail.com>
2017-02-13 23:28:54 -0800

    my commit message

--- app/src/main/java/darkfly0010/darknote/Screen1.java
+++ app/src/main/java/darkfly0010/darknote/Screen1.java
@@ -2,12 +2,21 @@ package darkfly0010.darknote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Screen1 extends AppCompatActivity {
-
+    TextView tv1;
+    EditText et1,et2;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
+    et1=(EditText)findViewById(R.id.et1);
+    et2=(EditText)findViewById(R.id.et2);
+    tv1=(TextView)findViewById(R.id.tv1);
+    b1=(Button)findViewById(R.id.b1);
     }
 }
